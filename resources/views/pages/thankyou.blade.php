ecfcc9d0b5fac22114e883403b78b4bdef4c6f2e
jyostna <ju36@njit.edu>
2018-02-25 14:45:45 -0500

    Thank you message - Dusted

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.default')
 @section('content')
-
-    <div class="card mt-3">
-        <div class="card-header">
+    <div class="container mt-3">
             <h3>Thank you {{$name}} for contacting us</h3>
-        </div>
-
+    </div>
 @endsection
